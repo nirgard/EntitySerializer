b4849ac71c09bc68e039eb5dd7693ed3820abb1a
nivwer <nivwerr@gmail.com>
2024-03-25 22:56:09 -0300

    created SimpleUserEntity

---
+++ tests/nivwer.EntityMapSerializer.Tests/Entities/SimpleUserEntity.cs
@@ -0,0 +1,7 @@
+namespace nivwer.EntityMapSerializer.Tests.Entities;
+
+public class SimpleUserEntity
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+}
