1fb1d96a4da9815d15d5259dd2a7cf4da1c230b6
nivwer <nivwerr@gmail.com>
2024-03-25 23:01:20 -0300

    updated PropertyMapper class

--- src/nivwer.EntityMapSerializer/PropertyMapper.cs
+++ src/nivwer.EntityMapSerializer/PropertyMapper.cs
@@ -6,25 +6,56 @@ namespace nivwer.EntityMapSerializer;
 
 public class PropertyMapper : IPropertyMapper
 {
-  
     public T MapDictionaryToEntity<T>(Dictionary<string, object?> map)
     where T : new()
     {
-        T entity = new();
-        PropertyInfo[] properties = typeof(T).GetProperties();
+        var Entity = MapDictionaryToEntity(typeof(T), map);
+
+        if (Entity == null)
+        {   
+            string message = "Failed to deserialize the entity.";
+            throw new InvalidOperationException(message);
+        }
+
+        return (T)Entity ?? new T();
+    }
+
+    private object? MapDictionaryToEntity(Type entityType, Dictionary<string, object?> map)
+    {
+        object? entity = Activator.CreateInstance(entityType);
+        PropertyInfo[] properties = entityType.GetProperties();
 
         foreach (PropertyInfo property in properties)
         {
             if (map.TryGetValue(property.Name, out object? value))
             {
-                if (TypeHelper.IsSimpleType(property.PropertyType))
-                {
-                    property.SetValue(entity, value);
-                }
+                SetValueBasedOnType(entity, property, value);
             }
         }
 
         return entity;
+    }
+
+    private void SetValueBasedOnType<T>(T entity, PropertyInfo property, object? value)
+    where T : new()
+    {
+        if (TypeHelper.IsSimpleType(property.PropertyType))
+        {
+            SetValueIfSimpleType(entity, property, value);
+        }
+        else if (TypeHelper.IsCollection(property.PropertyType))
+        {
+            // SetValueIfCollection(entity, property, value);
+        }
+        else
+        {
+            // SetValueForNestedObject(entity, property, value);
+        }
+    }
+
+    private void SetValueIfSimpleType<T>(T entity, PropertyInfo property, object? value)
+    {
+        property.SetValue(entity, value);
     }
 
     public Dictionary<string, object?> MapPropertiesToDictionary<T>(T entity)
