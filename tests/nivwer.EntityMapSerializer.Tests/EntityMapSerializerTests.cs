f0cd9b98e338fff67cc8ae4337c1623135cbbed5
nivwer <nivwerr@gmail.com>
2024-03-25 23:49:28 -0300

    added SerializeToMap_WithValidEntity_ReturnsMap method

--- tests/nivwer.EntityMapSerializer.Tests/EntityMapSerializerTests.cs
+++ tests/nivwer.EntityMapSerializer.Tests/EntityMapSerializerTests.cs
@@ -14,12 +14,13 @@ public class EntityMapSerializerTests
         EntityMapSerializer = new EntityMapSerializer();
     }
 
-    [DataRow(1, "John")]
+    [DataRow(1, "Jack")]
     [DataRow(2, "Alice")]
     [TestMethod]
     public void DeserializeFromMap_WithValidMap_ReturnsEntity(int id, string username)
     {
-        // Arrange
+        // -- Arrange ---------------------------------------------------
+
         Dictionary<string, object?> map = new Dictionary<string, object?>()
         {
             { "Id", id },
@@ -28,13 +29,43 @@ public class EntityMapSerializerTests
 
         SimpleUserEntity expected = new SimpleUserEntity() { Id = id, Username = username };
 
-        // Act
+        // -- Act -------------------------------------------------------
+
         SimpleUserEntity? actual = EntityMapSerializer?.DeserializeFromMap<SimpleUserEntity>(map);
 
-        // Assert
+        // -- Assert ----------------------------------------------------
+
         Assert.IsNotNull(actual);
         Assert.IsInstanceOfType(actual, typeof(SimpleUserEntity));
         Assert.AreEqual(expected.Id, actual.Id);
         Assert.AreEqual(expected.Username, actual.Username);
+    }
+
+    [DataRow(1, "Jack")]
+    [DataRow(2, "Alice")]
+    [TestMethod]
+    public void SerializeToMap_WithValidEntity_ReturnsMap(int id, string username)
+    {
+        // -- Arrange ---------------------------------------------------
+
+        SimpleUserEntity entity = new SimpleUserEntity() { Id = id, Username = username };
+
+        Dictionary<string, object?> expected = new Dictionary<string, object?>()
+        {
+            { "Id", id },
+            { "Username", username }
+        };
+
+        // -- Act -------------------------------------------------------
+
+        Dictionary<string, object?>? actual = EntityMapSerializer?.SerializeToMap(entity);
+
+        // -- Assert ----------------------------------------------------
+
+        Assert.IsNotNull(actual);
+        Assert.IsTrue(actual.ContainsKey("Id"));
+        Assert.IsTrue(actual.ContainsKey("Username"));
+        Assert.AreEqual(expected["Id"], actual["Id"]);
+        Assert.AreEqual(expected["Username"], actual["Username"]);
     }
 }
