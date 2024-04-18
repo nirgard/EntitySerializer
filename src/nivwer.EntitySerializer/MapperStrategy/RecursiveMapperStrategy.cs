9872ef35ff30a90ced9b089b721fdbda0d838ebf
nivwer <nivwerr@gmail.com>
2024-04-17 23:33:23 -0300

    add recursive mapper strategy

---
+++ src/nivwer.EntitySerializer/MapperStrategy/RecursiveMapperStrategy.cs
@@ -0,0 +1,51 @@
+using nivwer.EntitySerializer.Helpers;
+using nivwer.EntitySerializer.Interfaces;
+using nivwer.EntitySerializer.MapperStrategy.Interface;
+
+namespace nivwer.EntitySerializer.MapperStrategy;
+
+public class RecursiveMapperStrategy : IMapperStrategy
+{
+    private readonly IPropertyMapper PropertyMapper;
+
+    public RecursiveMapperStrategy(IPropertyMapper propertyMapper)
+    {
+        PropertyMapper = propertyMapper;
+    }
+
+    public object? MapValue(Type type, object? value)
+    {
+        if (type == typeof(object))
+            type = value?.GetType() ?? type;
+
+        IMapperStrategy strategy = GetMapperStrategy(type);
+        object? mappedValue = strategy.MapValue(type, value);
+
+        return mappedValue;
+    }
+
+    public object? UnmapValue(Type type, object? value)
+    {
+        if (type == typeof(object))
+            type = value?.GetType() ?? type;
+
+        IMapperStrategy strategy = GetMapperStrategy(type);
+        object? unmappedValue = strategy.UnmapValue(type, value);
+
+        return unmappedValue;
+    }
+
+    private IMapperStrategy GetMapperStrategy(Type type)
+    {
+        IMapperStrategy strategy;
+
+        if (TypeHelper.IsCollection(type))
+            strategy = new CollectionMapperStrategy(this);
+        else if (TypeHelper.IsDictionary(type))
+            strategy = new DictionaryMapperStrategy(this);
+        else
+            strategy = new SimpleMapperStrategy(PropertyMapper);
+
+        return strategy;
+    }
+}
