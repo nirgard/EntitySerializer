bdaaa30d8b4f679e6041667c8d22fbbde6a2153e
nivwer <nivwerr@gmail.com>
2024-04-18 20:13:04 -0300

    added support to dictionaries

--- src/nivwer.EntitySerializer/MapperStrategy/DictionaryMapperStrategy.cs
+++ src/nivwer.EntitySerializer/MapperStrategy/DictionaryMapperStrategy.cs
@@ -1,16 +1,16 @@
 using System.Collections;
-using nivwer.EntitySerializer.Interfaces;
+using nivwer.EntitySerializer.Helpers;
 using nivwer.EntitySerializer.MapperStrategy.Interface;
 
 namespace nivwer.EntitySerializer.MapperStrategy;
 
 public class DictionaryMapperStrategy : IMapperStrategy
 {
-    private readonly IPropertyMapper PropertyMapper;
+    private readonly IMapperStrategy RecursiveMapperStrategy;
 
-    public DictionaryMapperStrategy(IPropertyMapper propertyMapper)
+    public DictionaryMapperStrategy(IMapperStrategy recursiveMapperStrategy)
     {
-        PropertyMapper = propertyMapper;
+        RecursiveMapperStrategy = recursiveMapperStrategy;
     }
 
     public object? MapValue(Type type, object? value)
@@ -18,26 +18,65 @@ public class DictionaryMapperStrategy : IMapperStrategy
         Type keyType = GetDictionaryKeyType(type);
         Type valueType = GetDictionaryValueType(type);
 
-        if (value is IDictionary dictionary)
+        IDictionary? dictionary = value as IDictionary;
+
+        if (dictionary == null)
         {
-            Console.WriteLine($"Dictionary: {type}");
-            IDictionary<object, object?> mappedDictionary = new Dictionary<object, object?>();
-
-            foreach (DictionaryEntry entry in dictionary)
-            {
-                object mappedKey = PropertyMapper.MapValue(keyType, entry.Key)!;
-                object? mappedValue = PropertyMapper.MapValue(valueType, entry.Value);
-
-                mappedDictionary.Add(mappedKey, mappedValue);
-            }
-
-            return mappedDictionary;
-        }
-        else
-        {
-            Console.WriteLine($"Dictionary not IDictionary: {type}");
             return value;
         }
+
+        Console.WriteLine($"Map Dictionary: {type}");
+        IDictionary mappedDictionary = CreateDictionaryInstance(keyType, valueType, true);
+
+        foreach (DictionaryEntry entry in dictionary)
+        {
+            object mappedKey = RecursiveMapperStrategy.MapValue(keyType, entry.Key)!;
+            object? mappedValue = RecursiveMapperStrategy.MapValue(valueType, entry.Value);
+
+            mappedDictionary.Add(mappedKey, mappedValue);
+        }
+
+        return mappedDictionary;
+    }
+
+    public object? UnmapValue(Type type, object? value)
+    {
+        Type keyType = GetDictionaryKeyType(type);
+        Type valueType = GetDictionaryValueType(type);
+        
+        IDictionary? dictionary = value as IDictionary;
+
+        if (dictionary == null)
+        {
+            return value;
+        }
+
+        Console.WriteLine($"Unmap Dictionary: {type}");
+        IDictionary unmappedDictionary = CreateDictionaryInstance(keyType, valueType);
+
+        foreach (DictionaryEntry entry in dictionary)
+        {
+            object mappedKey = RecursiveMapperStrategy.UnmapValue(keyType, entry.Key)!;
+            object? mappedValue = RecursiveMapperStrategy.UnmapValue(valueType, entry.Value);
+
+            unmappedDictionary.Add(mappedKey, mappedValue);
+        }
+
+        return unmappedDictionary;
+    }
+
+    private IDictionary CreateDictionaryInstance(Type keyType, Type valueType, bool toMap = false)
+    {
+        if (toMap && TypeHelper.IsEntity(keyType))
+            keyType = typeof(Dictionary<string, object?>);
+
+        if (toMap && TypeHelper.IsEntity(valueType))
+            valueType = typeof(Dictionary<string, object?>);
+
+        Type dictionaryType = typeof(Dictionary<,>).MakeGenericType(keyType, valueType);
+        IDictionary dictionary = (IDictionary)Activator.CreateInstance(dictionaryType)!;
+
+        return dictionary;
     }
 
     private Type GetDictionaryKeyType(Type type)
